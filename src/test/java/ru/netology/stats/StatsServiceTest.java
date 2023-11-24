b53d9441e09598c59ad7489216b8810a7997b57a
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2023-11-24 16:01:38 +0300

    first commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,59 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+    @Test
+    public void shouldCountAmountSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 8+15+13+15+17+20+19+20+7+14+14+18;
+        long actuall = service.amountSales(sales);
+
+        Assertions.assertEquals(expected, actuall);
+    }
+    @Test
+    public void shouldCountAverageSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        long actuall = service.averageSales(sales);
+
+        Assertions.assertEquals(expected, actuall);
+    }
+    @Test
+    public void shouldCountMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 8;
+        long actuall = service.maxSales(sales);
+
+        Assertions.assertEquals(expected, actuall);
+    }
+    @Test
+    public void shouldCountMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 9;
+        long actuall = service.minSales(sales);
+
+        Assertions.assertEquals(expected, actuall);
+    }
+    @Test
+    public void shouldCountBelowAverageSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        long actuall = service.belowAverageSales(sales);
+
+        Assertions.assertEquals(expected, actuall);
+    }
+}
